f924ba248060c2a70487b83de6c6eb2d97dec8ac
Emre <112270755+emrrekus@users.noreply.github.com>
2022-12-01 23:48:38 +0300

    init
     if (!impact)
            {
    
                playerRb.velocity= new Vector3(0, 50*Time.fixedDeltaTime*5, 0);
    
            }

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    bool impact;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+
+            impact = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            impact = false;
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        if (impact)
+        {
+
+            playerRb.velocity = new Vector3(0, -100 * Time.fixedDeltaTime * 7, 0);
+        }
+         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!impact)
+        {
+
+            playerRb.velocity= new Vector3(0, 50*Time.fixedDeltaTime*5, 0);
+
+           
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!impact)
+        {
+
+            playerRb.velocity = new Vector3(0, 50 * Time.fixedDeltaTime * 5, 0);
+
+
+        }
+    }
+
+
+}
